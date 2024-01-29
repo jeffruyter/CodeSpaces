7480e6b94282f7ba56b8a78eb78abf70e4b304d6
jeffruyter <137963438+jeffruyter@users.noreply.github.com>
2024-01-29 09:12:22 +0000

    adding MyWorldSpaces and FSharpMyWorld dotnet code

---
+++ MyWorldCodeSpaces/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace MyWorldCodeSpace
+{
+    class Program
+
+    {
+        static void Main(string[] args)
+
+        {
+        Console.WriteLine("Hello, My World Githib CodeSpaces!");
+        }
+    }
+}
