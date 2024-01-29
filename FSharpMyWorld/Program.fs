3bd77a03d31573c3fe82c01bfe6c441c5187e246
jeffruyter <137963438+jeffruyter@users.noreply.github.com>
2024-01-29 09:04:56 +0000

    adding MyWorldSpaces and FSharpMyWorld dotnet code

---
+++ FSharpMyWorld/Program.fs
@@ -0,0 +1,15 @@
+﻿// For more information see https://aka.ms/fsharp-console-apps
+
+open System
+
+//Define a function to construct a message to print
+
+let from = fun whom ->
+    sprintf "from %s" whom
+[<EntryPoint>]
+
+let main argv =
+    let message = from "F#"  // Call the function
+    printfn "My World %s to Github CodeSpaces" message
+
+    0 // return an integer exit code 
